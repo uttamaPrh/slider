a350f1a00ebd29b4bd0ce9567f8c4db66d513a6d
Uttam Pradhan <uttampra1@gmail.com>
2024-03-11 23:57:44 +0545

    left float image with carousal our team

---
+++ card.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                cabinetGrotesk: "'Cabinet Grotesk', san-serif",
+            }
+        }
+    }
+}
+const slidesContainer = document.querySelector(".slides-container");
+const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+
+nextButton.addEventListener("click", () => {
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    slidesContainer.scrollLeft -= slideWidth;
+});
